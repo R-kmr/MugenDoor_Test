cb0acb605f51d784d2c9418e4968ffb38f4a9cce
R-kmr <re.070525@gmail.com>
2026-07-01 00:45:34 +0900

    6/29  扉の生成　修正
    ・ハンドトラッキングでの扉を開けた時に、次の扉が正しく生成されるようにした

--- VR_test0428/Assets/Script/GenerateDoor.cs
+++ VR_test0428/Assets/Script/GenerateDoor.cs
@@ -1,44 +1,56 @@
 using UnityEngine;
+using TMPro;
 
 public class GenerateDoor : MonoBehaviour
 {
-    Open open;
-    GameObject Door;
-    GameObject NewDoor;
+    public GameObject Door;
+    public  GameObject NewDoor;
+
+    int doorRnd;
 
     [SerializeField] GameObject RightSlide;
     [SerializeField] GameObject LeftSlide;
 
+    int Opened = 0;
+
+
+    [SerializeField] TextMeshProUGUI Text;
+
+    public bool openCheck;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
- 
+        //Door = GameObject.FindWithTag("Door");
     }
 
     // Update is called once per frame
     void Update()
     {
         Door = GameObject.FindWithTag("Door");
-        open = Door.GetComponent<Open>();
 
-
-        int doorRnd = Random.Range(1, 3);
-
-        switch (doorRnd)
+        if (Door.GetComponent<Open>().open)
         {
-            case 1:
-                NewDoor = RightSlide; break;
+            doorRnd = Random.Range(1, 3);
 
-            case 2:
-                NewDoor = LeftSlide; break;
-        }
+            switch (doorRnd)
+            {
+                case 1:
+                    NewDoor = RightSlide; break;
 
-        if (open.open)
-        {
-           // Debug.Log("В®Б[В’Вс");
+                case 2:
+                    NewDoor = LeftSlide; break;
+            }
+
+
             Instantiate(NewDoor);
-            //Door = NewDoor;
-            //open.open = false;
+            Destroy(Door);
+
+            
+            Opened++;
+            string opened = Opened.ToString();
+            Text.text = opened;
+
+            openCheck = false;
         }
     }
 }
